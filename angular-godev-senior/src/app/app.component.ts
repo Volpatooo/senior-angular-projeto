d6c70910da98f8e15cae5904b0ede5f4489522b1
Volpatooo <teteuvolpato@gmail.com>
2024-11-14 23:48:40 -0300

    Primeiros Exemplos usando primerng e angular

--- angular-godev-senior/src/app/app.component.ts
+++ angular-godev-senior/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CardModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Hello World!, Godev Senior';
+  title = 'Bom Jeour, Dev Senior from Senior Manager!!!';
 }
